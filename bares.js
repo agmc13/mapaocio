6ea97165e4dd2979ceddff93f9144d6bd45c8801
alfonso <alfonsomorenocordoba@gmail.com>
2017-10-09 19:05:51 +0200

    iconos de bares, restaurantes y autobuses añadidos

--- bares.js
+++ bares.js
@@ -1,11 +1,15 @@
+var BarIcon = L.icon({
+    iconUrl: 'bar.png',
+    iconSize:     [38, 50],
+});
 var bares = L.layerGroup();
-	L.marker([37.221887, -3.693409]).bindPopup('Heladeria Granada').addTo(bares),
-	L.marker([37.222229, -3.692545]).bindPopup('El rincón de Pepe el Güeno').addTo(bares),
-	L.marker([37.222852, -3.691115]).bindPopup('Bar el Paseo').addTo(bares),
-	L.marker([37.222981, -3.688257]).bindPopup('Bar Peña').addTo(bares),
-	L.marker([37.223469, -3.683350]).bindPopup('Bar Leones Atarfe <p><b>Tapa estrella:</b> Patata boquerones fritos+ Tubo de cerveza<p><b>Precio:</b> 1.70€</p></p>').addTo(bares),
-	L.marker([37.223598, -3.684000]).bindPopup('Bar El Caribe <p><b>Tapa estrella:</b> Patata asada casera + Tubo de cerveza<p><b>Precio:</b> 1.80€</p></p>').addTo(bares),
-	L.marker([37.217277, -3.691403]).bindPopup('Bar La Estela <p><b>Tapa estrella:</b> Pinchito + Tubo de cerveza<p><b>Precio:</b> 2€</p></p>').addTo(bares);
+	L.marker([37.221887, -3.693409],{icon: BarIcon}).bindPopup('Heladeria Granada').addTo(bares),
+	L.marker([37.222229, -3.692545],{icon: BarIcon}).bindPopup('El rincón de Pepe el Güeno').addTo(bares),
+	L.marker([37.222852, -3.691115],{icon: BarIcon}).bindPopup('Bar el Paseo').addTo(bares),
+	L.marker([37.222981, -3.688257],{icon: BarIcon}).bindPopup('Bar Peña').addTo(bares),
+	L.marker([37.223469, -3.683350],{icon: BarIcon}).bindPopup('Bar Leones Atarfe <p><b>Tapa estrella:</b> Patata boquerones fritos+ Tubo de cerveza<p><b>Precio:</b> 1.70€</p></p>').addTo(bares),
+	L.marker([37.223598, -3.684000],{icon: BarIcon}).bindPopup('Bar El Caribe <p><b>Tapa estrella:</b> Patata asada casera + Tubo de cerveza<p><b>Precio:</b> 1.80€</p></p>').addTo(bares),
+	L.marker([37.217277, -3.691403],{icon: BarIcon}).bindPopup('Bar La Estela <p><b>Tapa estrella:</b> Pinchito + Tubo de cerveza<p><b>Precio:</b> 2€</p></p>').addTo(bares);
 
 	var overlays = {
 		"Bares": bares
--- mapa.js
+++ mapa.js
@@ -1,22 +1,34 @@
+var BusIcon = L.icon({
+    iconUrl: 'bus.png',
+    iconSize:     [38, 50],
+});
+var BarIcon = L.icon({
+    iconUrl: 'bar.png',
+    iconSize:     [38, 50],
+});
+var RestauranteIcon = L.icon({
+    iconUrl: 'restaurante.png',
+    iconSize:     [38, 50],
+});
 function mapa() {
 		var locales = L.layerGroup();
 		var bares = L.layerGroup();
 		var autobus = L.layerGroup();
-		L.marker([37.218915, -3.696368]).bindPopup('Restaurante Casa Marino').addTo(locales),
-		L.marker([37.218013, -3.700221]).bindPopup('Restaurante Cortijo los Praos').addTo(locales),
-		L.marker([37.212593, -3.687610]).bindPopup('La Pará de Moradama').addTo(locales),
-		L.marker([37.220175, -3.688643]).bindPopup('El fogón de Rubén').addTo(locales),
-		L.marker([37.218005, -3.692215]).bindPopup('Pizzeria Santa Ana').addTo(locales),
-		L.marker([37.228623, -3.684637]).bindPopup('Asador de Pollos Avenida').addTo(locales),
-		L.marker([37.223422, -3.683707]).bindPopup('Mesón El Portón').addTo(locales),
-		L.marker([37.221887, -3.693409]).bindPopup('Heladeria Granada').addTo(bares),
-		L.marker([37.222229, -3.692545]).bindPopup('El rincón de Pepe el Güeno').addTo(bares),
-		L.marker([37.222852, -3.691115]).bindPopup('Bar el Paseo').addTo(bares),
-		L.marker([37.222981, -3.688257]).bindPopup('Bar Peña').addTo(bares),
-		L.marker([37.223469, -3.683350]).bindPopup('Bar Leones Atarfe').addTo(bares),
-		L.marker([37.223598, -3.684000]).bindPopup('Bar El Caribe <p><b>Tapa estrella:</b> Patata asada casera + tubo de cerveza<p><b>Precio:</b> 1.80€</p></p>').addTo(bares),
-		L.marker([37.217277, -3.691403]).bindPopup('Bar La Estela <p><b>Tapa estrella:</b> Pinchito + Tubo de cerveza<p><b>Precio:</b> 2€</p></p>').addTo(bares),
-		L.marker([37.224070, -3.681240]).bindPopup('Parada Autobus 112').addTo(autobus);
+		L.marker([37.218915, -3.696368],{icon: RestauranteIcon}).bindPopup('Restaurante Casa Marino').addTo(locales),
+		L.marker([37.218013, -3.700221],{icon: RestauranteIcon}).bindPopup('Restaurante Cortijo los Praos').addTo(locales),
+		L.marker([37.212593, -3.687610],{icon: RestauranteIcon}).bindPopup('La Pará de Moradama').addTo(locales),
+		L.marker([37.220175, -3.688643],{icon: RestauranteIcon}).bindPopup('El fogón de Rubén').addTo(locales),
+		L.marker([37.218005, -3.692215],{icon: RestauranteIcon}).bindPopup('Pizzeria Santa Ana').addTo(locales),
+		L.marker([37.228623, -3.684637],{icon: RestauranteIcon}).bindPopup('Asador de Pollos Avenida').addTo(locales),
+		L.marker([37.223422, -3.683707],{icon: RestauranteIcon}).bindPopup('Mesón El Portón').addTo(locales),
+		L.marker([37.221887, -3.693409],{icon: BarIcon}).bindPopup('Heladeria Granada').addTo(bares),
+		L.marker([37.222229, -3.692545],{icon: BarIcon}).bindPopup('El rincón de Pepe el Güeno').addTo(bares),
+		L.marker([37.222852, -3.691115],{icon: BarIcon}).bindPopup('Bar el Paseo').addTo(bares),
+		L.marker([37.222981, -3.688257],{icon: BarIcon}).bindPopup('Bar Peña').addTo(bares),
+		L.marker([37.223469, -3.683350],{icon: BarIcon}).bindPopup('Bar Leones Atarfe').addTo(bares),
+		L.marker([37.223598, -3.684000],{icon: BarIcon}).bindPopup('Bar El Caribe <p><b>Tapa estrella:</b> Patata asada casera + tubo de cerveza<p><b>Precio:</b> 1.80€</p></p>').addTo(bares),
+		L.marker([37.217277, -3.691403],{icon: BarIcon}).bindPopup('Bar La Estela <p><b>Tapa estrella:</b> Pinchito + Tubo de cerveza<p><b>Precio:</b> 2€</p></p>').addTo(bares),
+		L.marker([37.224070, -3.681240],{icon: BusIcon}).bindPopup('Parada Autobus 112').addTo(autobus);
 		var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
